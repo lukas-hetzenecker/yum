526e0f3dc39aea57ea703a7534c4062e39153fce
Lukas Hetzenecker <lukas.hetzenecker@gmail.com>
2013-11-27 01:39:21 +0100

    backport

--- resources/key.rb
+++ resources/key.rb
@@ -21,8 +21,8 @@
 actions :add, :remove
 default_action :add
 
-attribute :key, kind_of: String, name_attribute: true
-attribute :url, kind_of: String, default: nil
+attribute :key, :kind_of => String, :name_attribute => true
+attribute :url, :kind_of => String, :default => nil
 
 def initialize(*args)
   super
